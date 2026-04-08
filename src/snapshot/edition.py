a2ab319a59afc56069b105ded8984be9a08b80ed
xxyzz <gitpull@protonmail.com>
2026-04-08 12:25:30 +0800

    Add ru de editions

--- src/snapshot/edition.py
+++ src/snapshot/edition.py
@@ -2,4 +2,6 @@ EDITIONS = {
     "en": {"lang": "eng", "parsoid": [0]},
     "es": {"lang": "spa", "parsoid": [0]},
     "fr": {"lang": "fra", "parsoid": [0], "kiwix": ("Conjugaison:",)},
+    "ru": {"lang": "rus", "parsoid": [0]},
+    "de": {"lang": "deu", "parsoid": [0]},
 }
