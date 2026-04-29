77e08b51c76085b542337c597d5fed15ba62e833
xxyzz <gitpull@protonmail.com>
2026-04-29 16:36:52 +0800

    add zh edition

--- src/snapshot/edition.py
+++ src/snapshot/edition.py
@@ -4,4 +4,5 @@ EDITIONS = {
     "fr": {"lang": "fra", "parsoid": [0], "kiwix": ("Conjugaison:",)},
     "ru": {"lang": "rus", "parsoid": [0]},
     "de": {"lang": "deu", "parsoid": [0], "kiwix": ("Flexion:",)},
+    "zh": {"lang": "zho", "parsoid": [0]},
 }
